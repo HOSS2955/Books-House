7bbe3c8e2a1c2ab231a87e81a05d3ca9a81c7f71
MirnaMilad <mirnamilad0101@gmail.com>
2022-11-06 21:41:56 +0200

    second push

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MyNav from "./components/MyNav";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import BookDetails from "./components/BookDetails";
+import Login from "./components/login/Login";
+import PasswordPage from "./components/passwordPage/PasswordPage"
 
 function App() {
    return (
@@ -17,6 +19,8 @@ function App() {
             <Route path="/" element={<BooksShop />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/bookdetails/:id" element={<BookDetails />} />
+            <Route path="login" element={<Login/>}/>
+      <Route path="PasswordPage" element={<PasswordPage/>}/>
          </Routes>
          {/* <Counter /> */}
       </div>
