6e64aabb88231227464eb72e3ef33f2d84972992
Kareem Fahmy <kareemfahmyy@gmail.com>
2022-11-09 09:26:47 +0200

    Navigate Routes

--- src/App.js
+++ src/App.js
@@ -1,84 +1,80 @@
-import React , {useState} from "react";
-import { Route, Routes} from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import AuthRoutes from "./routes/AuthRoutes";
 import ComponentsRoutes from "./routes/ComponentsRoutes";
-import Login from "./authPages/login/Login"
-import PasswordPage from "./authPages/passwordPage/PasswordPage"
+import Login from "./authPages/login/Login";
+import PasswordPage from "./authPages/passwordPage/PasswordPage";
 import BooksShop from "./components/BooksShop";
 import Cart from "./components/Cart";
 import BookDetails from "./components/BookDetails";
-import ProtectedRoute from "./components/ProtectedRoute"
-
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useNavigate } from "react-router-dom";
 function App() {
-   // const user ={
-   //    id: '1',
-   //    name: 'robin',
-   // }
-   // const user = null
-   const [userData, setUserData] = useState(null);
-   const handleLogin = () => { 
-      setUserData({
-      id: '1',
-      name: 'robin',
-      // permissions: ['analyze'],
-      // roles: ['admin'],
-    })
-      
-   }
-    console.log(userData)
-   const handleLogout = () => setUserData({});
-   return (
-      <>
-      { userData? (<p>{userData.name}</p> ):<p>Hello</p>}
-       {userData? (
+  const navigate = useNavigate();
+  // const user ={
+  //    id: '1',
+  //    name: 'robin',
+  // }
+  // const user = null
+  const [userData, setUserData] = useState(null);
+  const handleLogin = () => {
+    setUserData({
+      id: "1",
+      name: "robin",
+    });
+    console.log("logged in");
+    navigate("/auth");
+  };
+  console.log(userData);
+  const handleLogout = () => {
+    setUserData({});
+    navigate("/login");
+  };
+  return (
+    <>
+      {userData ? <p>{userData.name}</p> : <p>Hello</p>}
+      {userData ? (
         <button onClick={handleLogout}>Sign Out</button>
       ) : (
         <button onClick={handleLogin}>Sign In</button>
       )}
-         <Routes>
-            <Route path="auth" element={<AuthRoutes />}>
-               <Route path="login" element={<Login />} />
-               <Route path="password" element={<PasswordPage />} />
-            </Route>
-            <Route path="components" element={
+      <Routes>
+        <Route path="auth" element={<AuthRoutes />}>
+          <Route path="login" element={<Login />} />
+          <Route path="password" element={<PasswordPage />} />
+        </Route>
+        <Route
+          path="components"
+          element={
             <ProtectedRoute
-            userData={userData}
-
-      //       user={{
-      //          id: '1',
-      // name: 'robin'
-      //       }}
-            redirectPath="/auth/password"
-            // isAllowed={
-            //   !!user && user.permissions.includes('analyze')
-            // }
-          >
-               <ComponentsRoutes/>
-               </ProtectedRoute>}>
-               <Route path="booksShop" element={<BooksShop />} />
-               <Route path="cart" element={<Cart />} />
-               <Route path="bookdetails/:id" element={<BookDetails />} />
-            </Route>
-            <Route path="*" element={<p>There's nothing here: 404!</p>} />
-         </Routes>
-      </>
-   );
+              userData={userData}
+              //       user={{
+              //          id: '1',
+              // name: 'robin'
+              //       }}
+              redirectPath="/auth/password"
+              // isAllowed={
+              //   //   !!user && user.permissions.includes('analyze')
+              // }
+            >
+              <ComponentsRoutes />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="booksShop" element={<BooksShop />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="bookdetails/:id" element={<BookDetails />} />
+        </Route>
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
 
 // import React from "react";
 // import { Route, Routes} from "react-router-dom";
@@ -95,7 +91,6 @@ export default App;
 // import BookDetails from "./components/BookDetails";
 // import ProtectedRoute from "./components/ProtectedRoute";
 // import {userActions} from "./store/reducers/userSlice"
-
 
 // function App() {
 //    const {user} = useSelector((state)=>state.user)
@@ -125,9 +120,9 @@ export default App;
 //             </Route>
 
 //             <Route path="components" element={
-               
+
 //             <ProtectedRoute
- 
+
 //             redirectPath="/auth/password"
 //             // isAllowed={
 //             //   !!user && user.permissions.includes('analyze')
--- src/store/reducers/userSlice.js
+++ src/store/reducers/userSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-   user: null
+  user: {},
 };
 
 const userSlice = createSlice({
-   name: "user",
-   initialState,
-   reducers: {
-    login: (state , action) =>{
-        state.user = action.payload
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
     },
-    logout:(state , action) =>{
-        state.user=action.payload
-    }
-   },
+    logout: (state, action) => {
+      state.user = action.payload;
+    },
+  },
 });
 
 export const userReducer = userSlice.reducer;
