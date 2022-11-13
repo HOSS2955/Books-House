f991bf7cb61bdb1412716e227a29782df5810e52
HOSS2955 <50694595+HOSS2955@users.noreply.github.com>
2022-11-13 22:44:51 +0200

    Enhancing UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import Cart from "./components/ui/Cart";
+import Cart from "./components/Cart/Cart";
 // import MyNav from "./components/MyNav";
 import Header from "./components/ui/Header";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,25 +11,31 @@ import Home from "./pages/Home";
 import BooksShop from "./pages/BookShop/index";
 import Footer from "./components/ui/Footer";
 import About from "./pages/About";
+import Service from "./pages/Service/Service";
+import Contactus from "./pages/Contactus/Contactus";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
-   return (
-      <div>
-         {/* <MyNav /> */}
-         <Header />
-         <ToastContainer />
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/bookdetails/:id" element={<BookDetails />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/booksshop" element={<BooksShop />} />
-         </Routes>
-         {/* <Counter /> */}
-         <Footer />
-      </div>
-   );
+  return (
+    <div>
+      {/* <MyNav /> */}
+      <Header />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/bookdetails/:id" element={<BookDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/booksshop" element={<BooksShop />} />
+      </Routes>
+      {/* <Counter /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ui/Aside.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Aside() {
+   return (
+      <div>
+         <h1 className="text-center fw-bold mt-5">Aside</h1>
+      </div>
+   );
+}
