cbd9b62311764c102d2a0c26277ecfbbda0fa7ee
HOSS2955 <50694595+HOSS2955@users.noreply.github.com>
2022-11-14 01:59:54 +0200

    fixing conflict

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
 import BooksShop from "./pages/BookShop/index";
-import Footer from "./components/ui/Footer";
-import About from "./pages/About";
 import Service from "./pages/Service/Service";
 import Contactus from "./pages/Contactus/Contactus";
-import Profile from "./pages/Profile/Profile/index";
+import Profile from "./pages/Profile/Profile";
 import Footer from "./components/ui/Footer";
 import About from "./pages/About";
 import AuthorsHouse from "./pages/AuthorsHouse";
@@ -22,39 +20,39 @@ import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
-   const [showWishlist, setShowWishlist] = useState(false);
-   const hideModal = () => {
-      setShowWishlist(false);
-   };
-   const showModal = () => {
-      setShowWishlist(true);
-   };
-   return (
-      <>
-         {/* <MyNav /> */}
-         <Header showModal={showModal} />
-         {showWishlist && (
-            <WishlistSideBar hideModal={hideModal} showModal={showModal} />
-         )}
-         <AnimatePresence
-            exitBeforeEnter
-            onExitComplete={() => setShowWishlist(false)}
-         >
-            <ToastContainer />
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/sidebar" element={<WishlistSideBar />} />
-               <Route path="/home" element={<Home />} />
-               <Route path="/cart" element={<Cart />} />
-               <Route path="/book/:id" element={<BookDetails />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/booksshop" element={<BooksShop />} />
-               <Route path="/authorshouse" element={<AuthorsHouse />} />
-            </Routes>
-            <Footer />
-         </AnimatePresence>
-      </>
-   );
+  const [showWishlist, setShowWishlist] = useState(false);
+  const hideModal = () => {
+    setShowWishlist(false);
+  };
+  const showModal = () => {
+    setShowWishlist(true);
+  };
+  return (
+    <>
+      {/* <MyNav /> */}
+      <Header showModal={showModal} />
+      {showWishlist && (
+        <WishlistSideBar hideModal={hideModal} showModal={showModal} />
+      )}
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => setShowWishlist(false)}
+      >
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sidebar" element={<WishlistSideBar />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/book/:id" element={<BookDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booksshop" element={<BooksShop />} />
+          <Route path="/authorshouse" element={<AuthorsHouse />} />
+        </Routes>
+        <Footer />
+      </AnimatePresence>
+    </>
+  );
 }
 
 export default App;
