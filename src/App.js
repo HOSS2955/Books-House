2a73a983670bf3ab93d79842c9f7cae3945a5cc5
HOSS2955 <50694595+HOSS2955@users.noreply.github.com>
2022-11-17 22:33:39 +0200

    Review Details

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import PasswordPage from "./pages/client/authPages/passwordPage/PasswordPage";
 import Login from "./pages/client/authPages/login/Login";
 import Verification from "./pages/client/authPages/verification/Verification";
 import { PrivateOutlet } from "./utils/privateRoutes";
+import ReviewDetails from "./pages/client/ReviewDetails/ReviewDetails";
 
 function App() {
   const [showWishlist, setShowWishlist] = useState(false);
@@ -66,6 +67,7 @@ function App() {
           <Route path="/profile" element={<MyProfile />} />
           <Route path="/booksshop" element={<BooksShop />} />
           <Route path="/authorshouse" element={<AuthorsHouse />} />
+          <Route path="/reviewdetails" element={<ReviewDetails />} />
         </Routes>
         {/* <Counter /> */}
         <Footer />
