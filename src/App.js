3236d4400c09889ad74e00788630dd20b2584003
mirnamilad <mirnamilad0101@gmail.com>
2022-11-21 23:06:59 +0200

    new

--- src/App.js
+++ src/App.js
@@ -19,12 +19,11 @@ import Footer from "./components/client/ui/Footer";
 
 import Contactus from "./pages/client/Contactus/Contactus";
 import About from "./pages/client/About";
-import RegisterPage2 from "./pages/client/authPages/register/regist";
+import Register from "./pages/client/authPages/register/Register";
 import ComponentsRoutes from "./routes/ComponentsRoutes";
 import AuthRoutes from "./routes/AuthRoutes";
 import VerifyPass from "./pages/client/authPages/passwordPage/VerifyPass";
-import Login2 from "./pages/client/authPages/login/log";
-
+import Login from "./pages/client/authPages/login/Login";
 import Verification from "./pages/client/authPages/verification/Verification";
 // import { PrivateOutlet } from "./utils/privateRoutes";
 import Reviews from "./pages/client/Review";
@@ -37,74 +36,69 @@ import RequireAuth from "./components/RequireAuth";
 import AlreadyUser from "./components/AlreadyUser";
 // import FullScreenLoader from "./components/FullScreenLoader";
 import Completion from "./components/client/ui/Completion/Completion";
-import ErrorNotFound from "./pages/client/errorNotFound";
 function App() {
-   const [showWishlist, setShowWishlist] = useState(false);
-   const hideModal = () => {
-      setShowWishlist(false);
-   };
-   const showModal = () => {
-      setShowWishlist(true);
-   };
-   return (
-      <div>
-         {/* <MyNav /> */}
-         <NavBar showModal={showModal} />
-         {showWishlist && (
-            <WishlistSideBar hideModal={hideModal} showModal={showModal} />
-         )}
-         <AnimatePresence
-            exitBeforeEnter
-            onExitComplete={() => setShowWishlist(false)}
-         >
-            <ToastContainer />
-            <Routes>
-               {/* Public Routes */}
-               <Route path="/" element={<Home />} />
-               <Route path="/sidebar" element={<WishlistSideBar />} />
-               <Route path="/home" element={<Home />} />
-               <Route
-                  element={<RequireAuth allowedRoles={["user", "admin"]} />}
-               >
-                  <Route path="/cart" element={<Cart />} />
-               </Route>
-               <Route path="/bookdetails/:id" element={<BookDetails />} />
-               <Route path="/@admin" element={<LoginAdmin />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/service" element={<Service />} />
-               <Route path="/contactus" element={<Contactus />} />
-               <Route path="/profile/settings" element={<MyProfile />} />
-               <Route path="/booksshop" element={<BooksShop />} />
-               <Route path="/authorshouse" element={<AuthorsHouse />} />
-               <Route path="/unauthorized" element={<UnauthorizePage />} />
-               <Route path="/reviews" element={<Reviews />} />
-               <Route path="/reviewdetails/:id" element={<ReviewDetails />} />
-               <Route path="/completion" element={<Completion />} />
-               {/* Protected Routes */}
-               <Route path="user/confirmEmail" element={<Verification />}>
-                  <Route path=":verificationCode" element={<Verification />} />
-               </Route>
-               <Route
-                  element={<RequireAuth allowedRoles={["user", "admin"]} />}
-               >
-                  <Route path="password" element={<VerifyPass />} />
-               </Route>
-               <Route element={<AlreadyUser />}>
-                  <Route path="auth" element={<AuthRoutes />}>
-                     <Route path="register" element={<Register />} />
+  const [showWishlist, setShowWishlist] = useState(false);
+  const hideModal = () => {
+    setShowWishlist(false);
+  };
+  const showModal = () => {
+    setShowWishlist(true);
+  };
+  return (
+    <div>
+      {/* <MyNav /> */}
+      <NavBar showModal={showModal} />
+      {showWishlist && (
+        <WishlistSideBar hideModal={hideModal} showModal={showModal} />
+      )}
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => setShowWishlist(false)}
+      >
+        <ToastContainer />
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/sidebar" element={<WishlistSideBar />} />
+          <Route path="/home" element={<Home />} />
+          <Route element={<RequireAuth allowedRoles={["user", "admin"]} />}>
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+          <Route path="/bookdetails/:id" element={<BookDetails />} />
+          <Route path="/@admin" element={<LoginAdmin />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contactus" element={<Contactus />} />
+          <Route path="/profile/settings" element={<MyProfile />} />
+          <Route path="/booksshop" element={<BooksShop />} />
+          <Route path="/authorshouse" element={<AuthorsHouse />} />
+          <Route path="/unauthorized" element={<UnauthorizePage />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/reviewdetails/:id" element={<ReviewDetails />} />
+          <Route path="/completion" element={<Completion />} />
+          {/* Protected Routes */}
+          <Route path="user/confirmEmail" element={<Verification />}>
+            <Route path=":verificationCode" element={<Verification />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={["user", "admin"]} />}>
+            <Route path="password" element={<VerifyPass />} />
+          </Route>
+          <Route element={<AlreadyUser />}>
+            <Route path="auth" element={<AuthRoutes />}>
+              <Route path="register" element={<Register />} />
 
-                     <Route path="login" element={<Login />} />
-                  </Route>
-               </Route>
-               {/* Catch All */}
-               //** محتاجين صفحة ايرور هنااا */
-               <Route path="*" element={<ErrorNotFound />} />
-            </Routes>
-            {/* <Counter /> */}
-            <Footer />
-         </AnimatePresence>
-      </div>
-   );
+              <Route path="login" element={<Login />} />
+            </Route>
+          </Route>
+          {/* Catch All */}
+          //** محتاجين صفحة ايرور هنااا */
+          {/* <Route path="*" element={<NoProducts />} /> */}
+        </Routes>
+        {/* <Counter /> */}
+        <Footer />
+      </AnimatePresence>
+    </div>
+  );
 }
 
 export default App;
