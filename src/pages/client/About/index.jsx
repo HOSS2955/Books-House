1b5750bee08a770e39b618f41f2c84cd42f2d83e
HOSS2955 <50694595+HOSS2955@users.noreply.github.com>
2022-11-26 03:03:29 +0200

    contact demo

--- src/pages/client/About/index.jsx
+++ src/pages/client/About/index.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import "../../../assets/css/About.css";
 import BreadCrumb from "../../../components/client/ui/Breadcrump/BreadCrumb";
 import AboutImg from "./AboutImg";
+import AboutTeam from "./AboutTeam";
 import OurGoals from "./OurGoals";
 
 const index = () => {
-   return (
-      <div>
-         {/* <BreadCrumb title="About" breadCrumb="About" /> */}
-         <div className="container">
-            <AboutImg />
-            <OurGoals />
-         </div>
+  return (
+    <div>
+      {/* <BreadCrumb title="About" breadCrumb="About" /> */}
+      <div className="container">
+        <AboutImg />
+        <OurGoals />
+        <AboutTeam />
       </div>
-   );
+    </div>
+  );
 };
 
 export default index;
