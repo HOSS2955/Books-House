ad9f940062404b54af6b87ad9dc1da44f2485fa4
HOSS2955 <50694595+HOSS2955@users.noreply.github.com>
2022-11-27 02:14:21 +0200

    from hossam

--- src/store/client/reducers/stripeOrdersSlice.js
+++ src/store/client/reducers/stripeOrdersSlice.js
@@ -2,31 +2,31 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-   stripeOrdersData: [],
+  stripeOrdersData: [],
 };
 
 export const getstripeOrders = createAsyncThunk(
-   "books/getBooks",
-   async (_, thunckAPI) => {
-      const { rejectWithValue } = thunckAPI;
-      try {
-         const response = await axios.get("/getallpayments/orders");
-         return response.data;
-      } catch (error) {
-         return rejectWithValue(error.message);
-      }
-   }
+  "/getallpayments/orders",
+  async (_, thunckAPI) => {
+    const { rejectWithValue } = thunckAPI;
+    try {
+      const response = await axios.get("/getallpayments/orders");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 export const stripeOrderslice = createSlice({
-   name: "stripeOrdersSlice",
-   initialState,
-   reducers: {},
-   extraReducers: {
-      [getstripeOrders.fulfilled]: (state, action) => {
-         state.stripeOrdersData = action.payload;
-      },
-   },
+  name: "stripeOrdersSlice",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getstripeOrders.fulfilled]: (state, action) => {
+      state.stripeOrdersData = action.payload;
+    },
+  },
 });
 
 export const stripeOrdersReducer = stripeOrderslice.reducer;
