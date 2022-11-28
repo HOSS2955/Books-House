4901ef15e83be3f15582714011388c08a5320de2
fady164 <76261718+fady164@users.noreply.github.com>
2022-11-28 02:10:19 +0200

    add route to package

--- routers/package.js
+++ routers/package.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {
-  addPackageData,
-  getAllPackages,
-  deleteAllPackages,
-  getPackageByID,
-  updatePackageById,
+   addPackageData,
+   getAllPackages,
+   deleteAllPackages,
+   getPackageByID,
+   updatePackageById,
 } = require("../controller/package.controller");
 const auth = require("../middelware/auth");
 
 router.post("/newpackage", addPackageData);
+router.put("/package/update/:id", updatePackageById);
 // router.get('/package/getall',auth,getAllPackages)
 router.get("/package/getall", getAllPackages);
 // router.get('/package/:id',auth,getPackageByID)
